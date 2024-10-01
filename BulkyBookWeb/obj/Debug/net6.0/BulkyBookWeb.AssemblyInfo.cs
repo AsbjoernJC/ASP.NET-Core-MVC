ab0e92a0f991da0a520fb10ccec2f98003989577
AsbjoernJC <asbjoernjc@gmail.com>
2024-10-01 10:22:13 +0200

    Finished project

--- BulkyBookWeb/obj/Debug/net6.0/BulkyBookWeb.AssemblyInfo.cs
+++ BulkyBookWeb/obj/Debug/net6.0/BulkyBookWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5aa834257b78d0e5b6123dc4b1c7dfb98217cb26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddada33e893bb733e597b093cdf1ec6efe18c25e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
